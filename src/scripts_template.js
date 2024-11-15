08cd18d1cdd99627cc70b3a7715b4e0778612651
gopalagabbita <gopala.gabbita@gmail.com>
2024-11-14 20:40:21 -0500

    ..

--- src/scripts_template.js
+++ src/scripts_template.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const apiGatewayEndpoint = "https://${api_id}.execute-api.${region}.amazonaws.com/visitor";
+    const apiGatewayEndpoint = "https://$${api_id}.execute-api.$${region}.amazonaws.com/visitor";
 
     fetch(apiGatewayEndpoint)
         .then(response => response.json())
-        .then(responseData => {  // This is a regular variable, so no ${} needed
+        .then(responseData => {
             document.getElementById("visitor-count").innerText = `Visitors: ${responseData.visitor_count}`;
         })
         .catch(error => console.error("Error fetching visitor count:", error));
