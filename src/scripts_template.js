007ac79b61ca960284d07699e528b177fc28b068
gopalagabbita <gopala.gabbita@gmail.com>
2024-11-14 20:38:44 -0500

    .

--- src/scripts_template.js
+++ src/scripts_template.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetch(apiGatewayEndpoint)
         .then(response => response.json())
-        .then(responseData => {  // Changed variable name here to avoid conflict
+        .then(responseData => {  // This is a regular variable, so no ${} needed
             document.getElementById("visitor-count").innerText = `Visitors: ${responseData.visitor_count}`;
         })
         .catch(error => console.error("Error fetching visitor count:", error));
